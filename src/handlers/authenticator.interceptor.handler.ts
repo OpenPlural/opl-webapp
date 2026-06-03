904b793e2d852fc711c07823d3bdbdb024c06557
Webbiii <webbiii@webbiii.cc>
2026-06-03 13:24:15 +0200

    Show toast when relogin is required

--- src/handlers/authenticator.interceptor.handler.ts
+++ src/handlers/authenticator.interceptor.handler.ts
@@ -3,9 +3,14 @@ import {catchError, EMPTY, Observable, throwError} from 'rxjs';
 import {inject} from '@angular/core';
 import {Router} from '@angular/router';
 import {AccountService} from '../services/AccountService';
+import {ToastService} from '../services/ToastService';
+import {TranslateService} from '@ngx-translate/core';
 
 export function authenticatedInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const translate = inject(TranslateService);
   const accountService = inject(AccountService);
+  const toastService = inject(ToastService);
+
   const account = accountService.account();
   if (account) {
     req = req.clone({
@@ -18,6 +23,9 @@ export function authenticatedInterceptor(req: HttpRequest<unknown>, next: HttpHa
     catchError(error => {
       if (error.status === 401 && !req.url.endsWith('/auth/delete-account') && !req.url.endsWith('/auth/change-password')) {
         if (router.url !== '/auth/login') {
+          const sessionExpiredMessage = translate.instant('session expired');
+          toastService.sendToast(sessionExpiredMessage, "alert-warning");
+
           accountService.logout();
           router.navigate(['auth', 'login']);
           return EMPTY;
