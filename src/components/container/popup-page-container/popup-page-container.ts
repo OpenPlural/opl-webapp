034e58f06d2ec26a00b2788196ffd7195b999938
Webbiii <webbiii@webbiii.cc>
2026-07-10 20:04:18 +0200

    Add search functionality to popup-page-container

--- src/components/container/popup-page-container/popup-page-container.ts
+++ src/components/container/popup-page-container/popup-page-container.ts
@@ -1,12 +1,13 @@
-import { Component, inject, input } from '@angular/core';
+import {Component, inject, input, output, signal} from '@angular/core';
 import { PageContainer } from '../page-container/page-container';
-import { Location } from '@angular/common';
+import {Location, NgClass} from '@angular/common';
 import { IconButton } from '../../icon-button/icon-button';
 import { VerticalCenter } from '../../vertical-center/vertical-center';
+import {ToggleIconButton} from '../../toggle-icon-button/toggle-icon-button';
 
 @Component({
   selector: 'app-popup-page-container',
-  imports: [PageContainer, IconButton, VerticalCenter],
+  imports: [PageContainer, IconButton, VerticalCenter, ToggleIconButton, NgClass],
   templateUrl: './popup-page-container.html',
 })
 export class PopupPageContainer {
@@ -14,6 +15,19 @@ export class PopupPageContainer {
 
   readonly title = input.required<string>();
   readonly footer = input<boolean>(false);
+  readonly searchable = input<boolean>(false);
+  readonly search = output<string | null>();
+
+  protected readonly searching = signal(false);
+
+  protected toggleSearch() {
+    this.searching.update((b) => {
+      if (b) {
+        this.search.emit(null);
+      }
+      return !b;
+    });
+  }
 
   protected goBack() {
     this.location.back();
