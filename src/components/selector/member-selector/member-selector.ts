78f57d8c674019735a631741bbeb9b163fa3e0c2
Webbiii <webbiii@webbiii.cc>
2026-07-20 18:38:22 +0200

    Add bulk folder member assignment (#112)

---
+++ src/components/selector/member-selector/member-selector.ts
@@ -0,0 +1,43 @@
+import { Component, computed, inject, input, OnInit, output, signal } from '@angular/core';
+import { LocalStorageService } from '../../../services/LocalStorageService';
+import { compareCustomSort } from '../../../util/CustomSort';
+import { Selector } from '../selector/selector';
+import { MemberListItem } from '../../list-item/member-list-item/member-list-item';
+import { MemberId } from '../../../services/model/Member';
+
+@Component({
+  selector: 'app-member-selector',
+  imports: [Selector, MemberListItem],
+  templateUrl: './member-selector.html',
+})
+export class MemberSelector implements OnInit {
+  private readonly localStorageService = inject(LocalStorageService);
+
+  readonly dialogId = input.required<string>();
+  readonly title = input.required<string>();
+  readonly custom = input.required<boolean>();
+  readonly selection = input<MemberId[]>([]);
+  readonly submitSelection = output<MemberId[]>();
+  readonly forceClose = output();
+
+  readonly updatedSelection = signal<MemberId[]>([]);
+
+  readonly members = computed(() => {
+    const custom = this.custom();
+    return this.localStorageService.members()
+      .filter(member => member.custom === custom)
+      .sort(compareCustomSort);
+  });
+
+  ngOnInit() {
+    this.updatedSelection.set(this.selection());
+  }
+
+  protected setSelected(id: MemberId, selected: boolean) {
+    if (selected) {
+      this.updatedSelection.update((selection) => [...selection, id]);
+    } else {
+      this.updatedSelection.update((selection) => selection.filter((v) => v !== id));
+    }
+  }
+}
