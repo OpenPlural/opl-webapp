2c6b8486c69b337b76c320e816d5ea3428939a5b
Webbiii <webbiii@webbiii.cc>
2026-07-21 02:41:05 +0200

    Add button to create member front entry

--- src/pages/app/member-front-history-page/member-front-history-page.ts
+++ src/pages/app/member-front-history-page/member-front-history-page.ts
@@ -1,10 +1,14 @@
 import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { Member } from '../../../services/model/Member';
-import { FrontEntry } from '../../../services/model/Front';
+import { FrontEntry, makeFrontEntry } from '../../../services/model/Front';
 import { WebService } from '../../../services/WebService';
 import { Loading } from '../../../components/loading/loading';
 import { TranslatePipe } from '@ngx-translate/core';
 import { HistoricFrontEntry } from '../../../components/historic-front-entry/historic-front-entry.component';
+import { openDialog } from '../../../util/CommonFunctions';
+import { truncateCurrentDate, truncateDate } from '../../../util/DateTruncate';
+import { LocalStorageService } from '../../../services/LocalStorageService';
+import { SyncService } from '../../../services/SyncService';
 
 @Component({
   selector: 'app-member-front-history-page',
@@ -12,6 +16,8 @@ import { HistoricFrontEntry } from '../../../components/historic-front-entry/his
   templateUrl: './member-front-history-page.html',
 })
 export class MemberFrontHistoryPage implements OnInit {
+  private readonly localStorageService = inject(LocalStorageService);
+  private readonly syncService = inject(SyncService);
   private readonly webService = inject(WebService);
 
   readonly member = input.required<Member>();
@@ -47,4 +53,38 @@ export class MemberFrontHistoryPage implements OnInit {
       this.loadingMore.set(false);
     }
   }
+
+  protected async createFrontTime(event: SubmitEvent) {
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const startTime = formData.get('startTime')?.toString().trim();
+    const endTime = formData.get('endTime')?.toString().trim();
+    form.reset();
+
+    if (startTime && endTime) {
+      const startTimestamp = Date.parse(startTime);
+      const endTimestamp = Date.parse(endTime);
+      const now = Date.now();
+
+      if (startTimestamp > endTimestamp || endTimestamp > now) {
+        return;
+      }
+
+      const startedAt = truncateDate(new Date(startTimestamp));
+      const endedAt = truncateDate(new Date(endTimestamp));
+
+      const frontEntry = makeFrontEntry(this.member().id);
+      await this.localStorageService.addFrontEntry({
+        ...frontEntry,
+        startedAt,
+        endedAt,
+      });
+      await this.syncService.fullSync();
+      this.frontHistory.set(null);
+      this.page.set(0);
+      await this.loadFrontHistory();
+    }
+  }
+
+  protected readonly openDialog = openDialog;
 }
