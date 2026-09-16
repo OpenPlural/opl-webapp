922cb42b55c805b894d7cdda9dc3850a0e28df93
Webbiii <webbiii@webbiii.cc>
2026-09-16 15:16:00 +0200

    Calculate initial period from end date

--- src/components/date-range/date-range.ts
+++ src/components/date-range/date-range.ts
@@ -15,7 +15,7 @@ export class DateRange implements OnInit {
 
   ngOnInit() {
     const end = new Date(Date.now() + 86400000);
-    const start = new Date(Date.now() - this.initialPeriod());
+    const start = new Date(end.getTime() - this.initialPeriod());
     this.start.set(start.toISOString().split('T')[0]);
     this.end.set(end.toISOString().split('T')[0]);
     this.emitDateRange();
